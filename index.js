46c7a7947c30b8d7bc6fbce2c0c52d1247e0eaee
Renato Dinhani Conceição <Renato Dinhani Conceio>
2017-05-02 23:39:52 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+// =============================================================================
+// DEPENDENCIES
+// =============================================================================
+const through = require('through2');
+const css = require('css');
+const PluginError = require('gulp-util').PluginError;
+
+// =============================================================================
+// CONSTANTS
+// =============================================================================
+const PLUGIN_NAME = 'gulp-css-attributes-remover';
+
+// =============================================================================
+// MODULE
+// =============================================================================
+module.exports = function (attributesToRemove) {
+
+    // VALIDATE
+    if (attributesToRemove === undefined || attributesToRemove === null) {
+        throw new PluginError(PLUGIN_NAME, 'Missing attributes to remove parameter.');
+    }
+    if (!Array.isArray(attributesToRemove)) {
+        throw new PluginError(PLUGIN_NAME, 'The attributes to remove shoud be an array. The passed argument is a ' + typeof attributesToRemove);
+    }
+
+    // INPUT
+    function parseInputCss(inputFile, encoding) {
+        let fileContent = inputFile.contents.toString(encoding);
+        let parsedCss = css.parse(fileContent);
+        return parsedCss;
+    }
+
+    // PARSING
+    function removeCssAttributes(parsedCss, attributesToRemove) {
+        parsedCss.stylesheet.rules = parsedCss.stylesheet.rules
+            .filter(rule => rule.type === 'rule')
+            .map(rule => {
+                rule.declarations = rule.declarations.filter(declaration => !attributesToRemove.includes(declaration.property));
+                return rule;
+            })
+        return parsedCss;
+    }
+
+    // OUTPUT
+    function outputFinalCss(modifiedCss) {
+        return css.stringify(modifiedCss);
+    }
+
+    // MAIN
+    let transform = function (file, encoding, callback) {
+        let parsedCss = parseInputCss(file, encoding);
+        let modifiedCss = removeCssAttributes(parsedCss, attributesToRemove)
+        let finalCss = outputFinalCss(modifiedCss)
+        file.contents = new Buffer(finalCss);
+
+        // success
+        callback(null, file);
+    }
+
+    //
+    return through.obj(transform);
+};
---
+++ test.js
@@ -0,0 +1,59 @@
+// =============================================================================
+// DEPENDENCIES
+// =============================================================================
+const File = require('vinyl');
+const assert = require('assert');
+const cssAttributeRemover = require('.');
+
+// =============================================================================
+// TESTES
+// =============================================================================
+describe('gulp-css-attribute-remover', function () {
+
+    // =========================================================================
+    // VALIDATIONS
+    // =========================================================================
+    it('fail on missing attributes', function (done) {
+        try {
+            let plugin = cssAttributeRemover()
+        } catch (err) {
+            done()
+        }
+    })
+
+    it('fail on not array attributes', function (done) {
+        try {
+            let plugin = cssAttributeRemover('font-family')
+        } catch (err) {
+            done()
+        }
+    })
+
+    // =========================================================================
+    // SUCCESS
+    // =========================================================================
+    it('should remove attributes', function (done) {
+        let plugin = cssAttributeRemover(['font-family', 'font-size']);
+        plugin.write(cssFile());
+
+        // wait for the file to come back out
+        plugin.once('data', function (file) {
+            let css = file.contents.toString();
+            assert(!css.includes('font-family'))
+            assert(!css.includes('font-size'))
+            done();
+        });
+    });
+
+});
+
+// =============================================================================
+// HELPERS
+// =============================================================================
+cssFile = function () {
+    let css = 'h1 {font-family:Times New Roman, font-weight:bold; font-size: 24px} p { font-family: Arial; font-weight: normal; font-size:12px}';
+
+    return new File({
+        contents: new Buffer(css)
+    })
+}
